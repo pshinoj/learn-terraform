65597a68743ac94a3e00e73e96cdd00a0821403f
sprabhakaran <shinoj.prabhakaran@philips.com>
2023-02-14 11:45:18 -0500

    updated kubernetes sample

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -19,3 +19,47 @@ resource "null_resource" "kubectl_kubegres" {
   }
 }
 
+resource "kubernetes_namespace" "tutorial" {
+  metadata {
+    annotations = {
+      name = "tutorial"
+    }
+
+    labels = {
+      mylabel = "tutorial"
+    }
+
+    name = "tutorial"
+  }
+}
+
+module "nginx_app" {
+  source = "./modules/nginx"
+  
+  namespace = "${kubernetes_namespace.tutorial.metadata.0.name}"
+
+  depends_on = [kubernetes_namespace.tutorial]
+}
+
+module "postgres_db" {
+  source = "./modules/kubegres"
+
+  namespace = "${kubernetes_namespace.tutorial.metadata.0.name}"
+  pg_admin_pwd = var.pg_admin_pwd
+  pg_repl_admin_pwd = var.pg_repl_admin_pwd
+
+  depends_on = [kubernetes_namespace.tutorial]
+}
+
+module "demo_app" {
+  source = "./modules/app"
+
+  namespace = "${kubernetes_namespace.tutorial.metadata.0.name}"
+  pg_hostname       = module.postgres_db.postgres_hostname
+  registry_server = var.registry_server
+  registry_email  = var.registry_email
+  registry_username = var.registry_username
+  registry_password = var.registry_password
+
+  depends_on = [module.postgres_db]
+}
