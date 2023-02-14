ba7b8b535fce201b151b160611d2ffa66a59581c
sprabhakaran <shinoj.prabhakaran@philips.com>
2023-02-14 11:38:35 -0500

    exercise answer key

--- basic/main.tf
+++ basic/main.tf
@@ -20,16 +20,36 @@ locals {
   prefix = "dev"
 }
 
-resource "local_file" "my_file" {
+resource "local_file" "my_file_ex1" {
   filename        = "${local.prefix}-${var.file_name}"
-  content         = "Random String - ${random_string.random[0].result}"
+  content         = "Random String - ${random_string.random_ex1.result}"
   file_permission = "0765"
 }
 
-resource "random_string" "random" {
-  count = 3
+variable "lengths" {
+  description = "Create random with varied lengths"
+  type        = list(number)
+  default     = [5, 8, 16]
+}
 
+resource "random_string" "random_ex1" {
   length           = 16
   special          = true
   override_special = "&@£$"                                                                                                                               1,11          Top
+}
+
+resource "random_string" "random" {
+  count            = length(var.lengths)
+
+  length           = var.lengths[count.index]
+  special          = true
+  override_special = "&@£$"
+}
+
+resource "local_file" "my_file" {
+  count = length(var.lengths)
+
+  filename        = "${local.prefix}${var.lengths[count.index]}-${var.file_name}"
+  content         = "Random String - ${random_string.random[count.index].result}"
+  file_permission = "0766"
 }
